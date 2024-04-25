a58d6a889bbbe870ce5130a50c9f12f1272c3b3a
dg015 <bruno.barbosa8@yahoo.com>
2024-04-25 15:49:33 -0300

    player controller done

--- knockback knockoff/Assets/scripts/PlayerController.cs
+++ knockback knockoff/Assets/scripts/PlayerController.cs
@@ -1,18 +1,74 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public BoxCollider2D bc;
+    public Rigidbody2D rb;
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float velocityMax;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float colisions;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(rb.velocity);
+
     }
+
+    private void FixedUpdate()
+    {
+        movement();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        colisions++;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        colisions--;
+    }
+
+    private void movement()
+    {
+
+        float Hspeed = Input.GetAxis("Horizontal") * speed;
+
+        if (Input.GetAxis("Horizontal") > -1.1)
+        {
+
+            rb.AddForce(new Vector2(Hspeed * speed, 0f));
+
+        }
+        if (colisions > 0)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, 15);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Debug.Log("jump");
+
+                rb.AddForce(new Vector2(0, jumpForce) * speed * Time.deltaTime, ForceMode2D.Impulse);
+                Debug.Log("pressed");
+            }
+        }
+        // sets the velocity based on a vector3 the clampsthe value inputed wihch is transformed into lenght only.
+
+    }
+
+
+
 }
