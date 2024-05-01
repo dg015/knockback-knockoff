770e907f4a9f99a4bf14efea90b607b6ab8ffb41
dg015 <bruno.barbosa8@yahoo.com>
2024-05-01 17:38:15 -0300

    Object now faces the right direction

--- knockback knockoff/Assets/scripts/AimAssist.cs
+++ knockback knockoff/Assets/scripts/AimAssist.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColiderKill : MonoBehaviour
+public class AimAssist : MonoBehaviour
 {
-    [SerializeField] private PolygonCollider2D col2d;
-    [SerializeField] private Rigidbody2D playerRb;
-    [SerializeField] private float force;
     [SerializeField] private Transform target;
     [SerializeField] private float targetAngle;
+
+    [SerializeField] private Transform Area;
+    [SerializeField] private float  angle;
+
+    [SerializeField] private Rigidbody2D playerRb;
+    private Vector2 lastPosition;
+
+    [SerializeField] private float offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
+
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        /*
+     /*   
         if (collision.CompareTag("Player"))
         {
             Transform targetPosition = collision.GetComponent<Transform>();
@@ -32,17 +41,35 @@ public class ColiderKill : MonoBehaviour
 
             //transform.rotation = Quaternion.Euler(0, 0, direction);
             
-        }*/
+        }
+     */
     }
 
-
-    private float angle()
+    private void FindDirection()
     {
-        return targetAngle;
+        // Calculate the current velocity of the player
+        Vector2 currentPosition = playerRb.position;
+        Vector2 currentVelocity = (currentPosition - lastPosition) / Time.deltaTime;
+        lastPosition = currentPosition;
+
+        // Determine the direction of movement based on velocity
+        Vector2 movementDirection = currentVelocity.normalized;
+   
+
+
+
+        // Calculate the angle of movement
+        float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
+        
+        // Apply the rotation to the "Area" transform
+        Area.rotation = Quaternion.Euler(0, 0, angle + offset);
+
     }
+
     // Update is called once per frame
     void Update()
     {
-        
+
+        FindDirection();
     }
 }
