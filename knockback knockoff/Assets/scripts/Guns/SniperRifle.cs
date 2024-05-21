4c3fb9f77f40ef079753c18d5bec7c18b1ea5e1b
dg015 <bruno.barbosa8@yahoo.com>
2024-05-21 10:07:58 -0300

    sniper added

---
+++ knockback knockoff/Assets/scripts/Guns/SniperRifle.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class SniperRifle : Gun
+{
+
+    [SerializeField] private CinemachineVirtualCamera Cinemachine;
+    [SerializeField] private CinemachineBasicMultiChannelPerlin shake;
+    [SerializeField] private float shakeIntensity;
+    [SerializeField] private float Shaketime;
+    [SerializeField] private float shaketimeMax;
+    [SerializeField] private bool readyToFire = true;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        pivot = gameObject.transform.GetComponentInParent<Transform>();
+        playerRb = gameObject.transform.GetComponentInParent<Rigidbody2D>();
+        Cinemachine = GameObject.Find("Virtual Camera").GetComponent<CinemachineVirtualCamera>();
+        shake = Cinemachine.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        barrel = GameObject.Find("barrel").GetComponent<Transform>();
+    }
+
+    private void Awake()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        aim();
+        CameraShakeTimer();
+        if (Input.GetMouseButton(0) && readyToFire)
+        {
+            StartCoroutine(shootingCycle());
+        }
+
+    }
+
+
+    public void CameraShake()
+    {
+        //add camera shake 
+        shake.m_AmplitudeGain = shakeIntensity;
+    }
+
+    private void CameraShakeTimer()
+    {
+        // check if theres any shake on the camerea
+        if (shake.m_AmplitudeGain > 0)
+        {
+            //increase shake time
+            Shaketime += Time.deltaTime;
+            if (Shaketime > shaketimeMax)
+            {
+                //set camera shake to 0 
+                shake.m_AmplitudeGain = 0;
+                Shaketime = 0;
+            }
+
+        }
+    }
+
+    protected override void shoot()
+    {
+        playerRb.AddForce(-1 * angle * force, ForceMode2D.Force);
+        CameraShake();
+    }
+
+    private IEnumerator shootingCycle()
+    {
+
+        shoot();
+        readyToFire = false;
+        yield return new WaitForSeconds(timeBetweenShots);
+        Debug.Log("I'me here");
+        readyToFire = true;
+
+
+    }
+}
