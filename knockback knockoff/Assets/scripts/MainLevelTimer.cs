7f610452b6d2cd5c1ab0481ab9f9009aeeeb140c
dg015 <bruno.barbosa8@yahoo.com>
2025-08-16 17:12:42 -0400

    added a timer

---
+++ knockback knockoff/Assets/scripts/MainLevelTimer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MainLevelTimer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI TimerText;
+    float elaspedTime;
+    public bool stopTimer;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        if(!stopTimer)
+            increaseTime();
+        setText();
+        int minutes = Mathf.FloorToInt(elaspedTime / 60);
+        int seconds = Mathf.FloorToInt(elaspedTime % 60);
+        TimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    private void increaseTime()
+    {
+        elaspedTime += Time.deltaTime;
+
+    }
+
+    private void setText()
+    {
+        TimerText.text = elaspedTime.ToString();
+    }
+
+}
