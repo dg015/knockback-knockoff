e0762e6fbd15229ceb764a2da595f7d9f87d6dc3
dg015 <bruno.barbosa8@yahoo.com>
2026-01-10 17:49:54 -0500

    Added fireball
    -Added fireball to player
    -speechecker script now can enable and disable fireball based on the speed of the player
    -speedchecker changes the trasnparency of the fireball based on the player speed

--- knockback knockoff/Assets/scripts/Player/SpeedChecker.cs
+++ knockback knockoff/Assets/scripts/Player/SpeedChecker.cs
@@ -9,7 +9,12 @@ public class SpeedChecker : MonoBehaviour
     [SerializeField] private PlayerController playerController;
     [SerializeField] private Image speedBar;
     public bool KillSpeed;
+    [SerializeField] private GameObject fireball;
+    [SerializeField] private SpriteRenderer fireballSpriteRenderer;
+    [SerializeField] private float fireballTransparency;
+    [SerializeField] private float maxTransperency;
 
+    [SerializeField] private float startFireBallSpeed;
     [SerializeField] private float maxspeed;
     // Start is called before the first frame update
     void Start()
@@ -25,6 +30,8 @@ public class SpeedChecker : MonoBehaviour
 
         fill();
         killSpeed();
+        startFireball();
+        fireballIntensitiy();
     }
 
     private void killSpeed()
@@ -33,7 +40,7 @@ public class SpeedChecker : MonoBehaviour
         {
             KillSpeed = true;
             Debug.Log("kill speed");
-
+           
         }
         else
         {
@@ -43,12 +50,40 @@ public class SpeedChecker : MonoBehaviour
 
     }
 
-    
+    private void startFireball()
+    {
+        
+        if(playerController.PVelocity.magnitude/ maxspeed >= startFireBallSpeed/maxspeed)
+        {
+            Debug.Log("started");
+            fireball.SetActive(true);
+            
+        }
+        else
+        {
+            fireball.SetActive(false);
+        }
+
+    }
+
+    //transparancy max 150
+    //transparancy min 50
+    private void fireballIntensitiy()
+    {
+        
+        //normalize both 
+        fireballTransparency = (playerController.PVelocity.magnitude / maxspeed) * (maxTransperency / 255);
+        //fireballTransparency = Mathf.Clamp(fireballTransparency, 50 / 255, maxTransperency / 255);
+        fireballSpriteRenderer.color = new Color(1f, 1f, 1f, fireballTransparency);
+       
+    }
+
 
     private void fill()
     {
         
         speedBar.fillAmount = Mathf.Clamp01(playerController.PVelocity.magnitude / maxspeed);
+
     }
 
 }
