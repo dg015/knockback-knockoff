64e4292925902e79e690679a5208accc7eac70fc
dg015 <bruno.barbosa8@yahoo.com>
2026-04-06 11:17:09 -0400

    Started working on main menu

---
+++ knockback knockoff/Assets/scripts/UI/MainMenuUI.cs
@@ -0,0 +1,54 @@
+using FMODUnity;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private StudioEventEmitter hover;
+    [SerializeField] private StudioEventEmitter click;
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        hover.Play();
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void singlePlayerChosen()
+    {
+        //load scene
+        Invoke(nameof(singlePlayerChosen), 3f);
+        //play audio saying "knockback knockof" kinda like resident evil
+    }
+
+    public void loadSinglePlayer()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void loadMultiplayer()
+    {
+
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
